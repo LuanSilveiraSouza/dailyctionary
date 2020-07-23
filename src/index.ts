84d588d0c15935d329661335f422b0334abd923a
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2020-07-23 16:16:31 -0300

    First WebScrap

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,23 @@ const puppeteer = require('puppeteer');
 console.log('WebScraping!!!');
 
 const makePdf = async (url: string) => {
-  const browser = await puppeteer.launch({});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
   await page.goto(url);
-  await page.screenshot({ path: 'print.png' });
+  await page.type('input#query', 'hola');
+  await page.click('button');
+
+  await page.waitFor(1000);
+
+  const result = await page.$eval(
+    'section.gramb ul li div p span.ind',
+    (el: any) => el.innerHTML
+  );
+
+  console.log(result);
+
   await browser.close();
 };
 
-makePdf('https://www.google.com.br/');
+makePdf('https://www.lexico.com/es');
