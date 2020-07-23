0d0981bbe19cb10fa988d9ff34cb3c1b8fd2c744
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2020-07-23 14:42:19 -0300

    Setting up Puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!!!");
+const puppeteer = require('puppeteer');
+
+console.log('WebScraping!!!');
+
+const makePdf = async (url: string) => {
+  const browser = await puppeteer.launch({});
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.screenshot({ path: 'print.png' });
+  await browser.close();
+};
+
+makePdf('https://www.google.com.br/');
