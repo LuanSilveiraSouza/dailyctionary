29e14e326743b2645c29bf364e9bf0ff8d5a33cb
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2020-07-28 13:05:01 -0300

    Completing word scraping

--- __tests__/integration/SpanishScraper.test.ts
+++ __tests__/integration/SpanishScraper.test.ts
@@ -9,9 +9,15 @@ describe('Spanish scraper tests', () => {
       .getWordEntry(SpanishWord.create('ampolleta'))
       .then((word) => {
         expect(word.name).toBe('ampolleta');
+        expect(word.type).toBe('FEMININE NOUN');
+        expect(word.definitions).toEqual(
+          expect.arrayContaining([
+            'Reloj de arena, especialmente el usado por los marinos.',
+            'Ampolla de cristal en cuyo interior, en el que se ha hecho el vacío, hay un filamento que produce luz por incandescencia al paso de una corriente eléctrica.',
+            'Ampolla (recipiente pequeño de cristal)',
+          ])
+        );
         expect(word.url).toBe('https://www.lexico.com/es/definicion/ampolleta');
       });
-
-    //expect(word.url).toBe('https://www.lexico.com/es/definicion/ampolleta');
   }, 15000);
 });
--- src/usecase/SpanishScraper.ts
+++ src/usecase/SpanishScraper.ts
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
 import { Scraper } from './Scraper';
 import { Word } from '../domain/Word';
+import { defaultMaxListeners } from 'stream';
 
 export class SpanishScraper implements Scraper {
   readonly baseUrl: string;
@@ -17,7 +18,21 @@ export class SpanishScraper implements Scraper {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(`${this.baseUrl}/definicion/${word.name}`);
-    
+
+    const definitions = await page.$$('ul.semb li div.trg p span.ind');
+
+    for (const def of definitions) {
+      const singleDef = await page.evaluate(el => el.innerText, def);
+
+      word.definitions?.push(singleDef);
+    }
+
+    word.type = '';
+    word.type += await page.$eval(
+      'section.gramb h3.ps span.pos',
+      (element: any) => element.innerHTML.toUpperCase()
+    );
+
     word.url = await page.url();
 
     await browser.close();
